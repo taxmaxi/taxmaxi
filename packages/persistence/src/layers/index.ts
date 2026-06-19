bd51663880b7d1b5a7a246e6c64d873064c4edd3
Max Ast <maxast31@gmail.com>
2026-06-19 08:27:00 +0300

    fix(persistence): remove unneeded ProtocolTransactionTypeMappingRepositoryLive export in barrel file

--- packages/persistence/src/layers/index.ts
+++ packages/persistence/src/layers/index.ts
@@ -1,4 +1,3 @@
 export * from "./RepositoriesLive.ts"
 export * from "./PgClientLive.ts"
 export * from "./ProtocolCandidateRepositoryLive.ts"
-export * from "./ProtocolTransactionTypeMappingRepositoryLive.ts"
