1ce1ae65ee683ccd7fdc2d3fe4db35e0fceef7d1
Max Ast <maxast31@gmail.com>
2026-08-14 14:44:20 +0200

    fix(rest-api): remove unused billing layer export

--- packages/rest-api/src/index.ts
+++ packages/rest-api/src/index.ts
@@ -1,5 +1,4 @@
 export { TaxMaxiApiLive } from "./layers/TaxMaxiApiLive.ts"
-export { StripeBillingServiceLive } from "./layers/StripeBillingServiceLive.ts"
 export { SessionTokenValidatorLive } from "./layers/AuthApiLive.ts"
 export { invalidSessionCookieCleanup } from "./layers/AuthMiddlewareLive.ts"
 export { AnonSessionServiceLive } from "./layers/AnonSessionServiceLive.ts"
